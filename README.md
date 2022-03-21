39ac46f7866944d644758250735fea3ed1b0ee17
Rene Hernandez <rhernandez@onenorth.com>
2022-03-20 22:30:13 -0500

    part 1 and create streak counter mock

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# streak-counter
+# `@jsjoeio/streak-counter` - a basic streak counter
+
+This is a basic streak counter - inspired by Duolingo - written in Typescript and meant for the browser (uses `localstorage`).
+
+## install
+
+```shell
+yarn add @jsjoeio/streak-counter
+
+```
---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import { formattedDate } from "./utils";
+
+interface Streak {
+  currentCount: number
+  startDate: String
+  lastLoginDate: string
+}
+const KEY = 'streak'
+
+export function streakCounter(storage:Storage, date: Date): Streak {
+
+  const streakInLocalStorage = storage.getItem(KEY);
+  
+  if (streakInLocalStorage) {
+    try {
+      const streak = JSON.parse(streakInLocalStorage || "");
+
+      return streak;
+    } catch (error) {
+      console.error("Failed to parse streak from localStorage");
+    }
+  }
+
+  const streak = {
+    currentCount: 1,
+    startDate: formattedDate(date),
+    lastLoginDate: formattedDate(date),
+  }
+  // store in localstorage
+  storage.setItem(KEY, JSON.stringify(streak))
+
+  return streak
+}
